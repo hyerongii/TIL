8a77a502f35388330c3ef3dded17dc04a3ff4463
hyerongii <heyrong22@gmail.com>
2024-10-07 17:29:06 +0900

    update_1007

--- Django/06-form/articles/views.py
+++ Django/06-form/articles/views.py
@@ -9,6 +9,7 @@ def index(request):
     articles = Article.objects.all()
     context = {
         'articles': articles,
+        
     }
     return render(request, 'articles/index.html', context)
 
