3b7a2e124e6c786cc2f0e2b7dabc72a51e596796
hyerongii <heyrong22@gmail.com>
2024-11-06 11:49:56 +0900

    update_1106

---
+++ Vue/03-single-file-components/MyComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="greeting">
+    {{ msg }}
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  const msg = ref('hello')
+  // 리턴 따로 필요없음
+</script>
+
+<!-- scoped: 해당 template 영역만 style 적용하겠다는 선언 -->
+<style scoped>
+  .greeting {
+    color:red;
+  }
+</style>
