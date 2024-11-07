57ad813188456d639c9234dead23ae8af1cd0791
hyerongii <heyrong22@gmail.com>
2024-11-07 11:02:21 +0900

    update_1107

---
+++ Vue/04-component-state-flow/vue-project/src/components/Parent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <ParentChild 
+      my-msg="message"
+      :dynamic-props="name"
+      @some-event="someCallback"
+      @emit-args="getNumbers"
+      @update-name="updateName"
+    />
+    <ParentItem
+      v-for="item in items"
+      :key="item.id"
+      :my-prop="item"
+    />
+  </div>
+</template>
+
+<script setup>
+import ParentChild from '@/components/ParentChild.vue'
+import {ref} from 'vue'
+import ParentItem from '@/components/ParentItem.vue'
+
+const name = ref('Alice')
+const items = ref([
+  {id:1, name: '사과'},
+  {id:2, name: '바나나'},
+  {id:3, name: '딸기'}
+])
+
+const someCallback = function () {
+  console.log('ParentChild가 발신한 이벤트를 수신했어요')
+}
+
+const getNumbers = function (...args) {
+  console.log(args)
+}
+
+const updateName = function () {
+  name.value = 'Bella'
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue/04-component-state-flow/vue-project/src/components/ParentGrandChild.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{ myMsg }}</p>
+    <button @click="updateName">이름 변경</button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  myMsg:String,
+})
+
+//  이름 변경 요청하는 이벤트 발신
+
+const emit = defineEmits(['updateName'])
+
+const updateName = function () {
+  emit('updateName')
+}
+</script>
+
+<style scoped>
+
+</style>
